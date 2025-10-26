df85315b5847e57b3075ae911b93528b2116a75c
emirusmaximus <cobcompan@gmail.com>
2025-10-27 01:12:04 +0300

    Update supabaseClient.ts

--- lib/supabaseClient.ts
+++ lib/supabaseClient.ts
@@ -2,16 +2,5 @@ import { createClient } from "@supabase/supabase-js";
 
 export const supabase = createClient(
   process.env.NEXT_PUBLIC_SUPABASE_URL as string,
-  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY as string,
-  {
-    auth: {
-      persistSession: true,        // ✅ Kullanıcı oturumu tarayıcıda saklanır
-      autoRefreshToken: true,      // ✅ Token otomatik yenilenir
-      detectSessionInUrl: true,
-      storage:
-        typeof window !== "undefined"
-          ? window.localStorage
-          : undefined,
-    },
-  }
+  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY as string
 );
